437fd351a44419783064ceeaf914f85668fd8f98
Pramika <pramikae464@gmail.com>
2026-06-11 15:43:01 +0530

    Initialize Django project setup

--- contracts/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-def home(request):
-    return JsonResponse({
-        "message": "Contract Parsing API is running"
-    })from django.shortcuts import render
-
-# Create your views here.
