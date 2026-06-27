5e59aa33f9c6f10b6439cf6cabc172d58a415003
purnimasahoo07 <purnimasahoo395@gmail.com>
2026-06-27 21:18:11 +0530

    Added document listing API

--- contracts/views.py
+++ contracts/views.py
@@ -1,8 +1,22 @@
 from django.http import JsonResponse
+from .models import Document
 from django.shortcuts import render
 def home(request):
     return JsonResponse({
         "message": "Contract Parsing API is running"
     })
 
-# Create your views here.
+def document_list(request):
+    documents = Document.objects.all()
+
+    data = []
+
+    for doc in documents:
+        data.append({
+            "id": doc.id,
+            "name": doc.name,
+            "status": doc.status,
+            "uploaded_at": doc.uploaded_at
+        })
+
+    return JsonResponse(data, safe=False)
