d17513cf90a9432508ebf56b506ca1799f7d9aa7
Rohin Cyril <rohincyril2000@gmail.com>
2026-07-06 11:16:29 +0530

    pdf extraction using pymupdf

---
+++ utility function/utils.py
@@ -0,0 +1,17 @@
+class PDFUtils:
+
+    @staticmethod
+    def extract_text(pdf_path):
+        ...
+
+    @staticmethod
+    def extract_metadata(pdf_path):
+        ...
+
+    @staticmethod
+    def get_page_count(pdf_path):
+        ...
+
+    @staticmethod
+    def search_text(pdf_path, keyword):
+        ...
