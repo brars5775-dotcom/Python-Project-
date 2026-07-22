c28ae316de89b9c14d93b74c0cc4b14c8b91d1fc
Rohin Cyri <rohincyril2000@gmail.com>
2026-07-22 10:37:40 +0530

    Initialize Django Project Setup and Django REST Framework installation

--- contracts/urls.py
+++ contracts/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
-from .views import home
+from .views import (
+     home,
+     upload_contract,
+     list_documents,
+     view_clauses,
+     view_risks
+) 
 
 urlpatterns = [
-    path('', home),
+    path("", home),
+    path("upload/", upload_contract),
+    path("documents/", list_documents),
+    path(
+        "clauses/<int:document_id>/",
+        view_clauses
+    ),
+    path(
+        "risks/<int:document_id>/",
+        view_risks
+    ),
 ]
